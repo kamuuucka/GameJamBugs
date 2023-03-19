922c1931fab8cdd9ed7caa18fd0d13e49593bcc9
Kamila <kamila.matuszak@list.pl>
2023-03-19 03:52:11 +0100

    Sounds in level1

--- GameJamBugs/Assets/Scripts/Buttons.cs
+++ GameJamBugs/Assets/Scripts/Buttons.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class Buttons : MonoBehaviour
 {
     [SerializeField] private Image notes;
+    [SerializeField] private AudioManager audioManager;
     
     public void OpenOverlay()
     {
         notes.gameObject.SetActive(true);
+        audioManager.Play("button");
     }
 
     public void CloseOverlay()
     {
         notes.gameObject.SetActive(false);
+        audioManager.Play("button");
     }
 }
